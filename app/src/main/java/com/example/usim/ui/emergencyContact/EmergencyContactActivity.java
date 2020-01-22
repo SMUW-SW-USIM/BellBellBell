e6bf203970924f14afc9d03307aa06a2e6720509
DaEunShim <10ekdms01@naver.com>
2020-01-22 19:51:04 +0900

    color change

--- app/src/main/java/com/example/usim/ui/emergencyContact/EmergencyContactActivity.java
+++ app/src/main/java/com/example/usim/ui/emergencyContact/EmergencyContactActivity.java
@@ -1,14 +1,14 @@
 package com.example.usim.ui.emergencyContact;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.usim.R;
-import com.example.usim.ui.record.RecordAddActivity;
 
 public class EmergencyContactActivity extends AppCompatActivity {
 
@@ -28,7 +28,6 @@ public class EmergencyContactActivity extends AppCompatActivity {
             }
         });
 
-        @Override
-        protected void onActivityResult(int requestCode)
+
     }
 }
