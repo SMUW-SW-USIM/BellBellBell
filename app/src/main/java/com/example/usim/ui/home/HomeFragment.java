d6e3a1cdbcc0104f77da1565bbdf79fbae83e1cb
canola78 <yce9110@sookmyung.ac.kr>
2020-02-02 18:12:44 +0900

    add btn on home

--- app/src/main/java/com/example/usim/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/usim/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.usim.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,9 +15,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.usim.R;
+import com.example.usim.ui.emergencyContact.EmergencyContactActivity;
+import com.example.usim.ui.record.RecordAddActivity;
 
 public class HomeFragment extends Fragment {
 
+    Button btnRecord, btnEmergency;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -32,8 +37,24 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate( R.layout.fragment_home, container, false );
+        btnRecord = (Button)view.findViewById(R.id.btnRecord);
+        btnEmergency  = (Button)view.findViewById(R.id.btnEmergemcy);
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), RecordAddActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EmergencyContactActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
