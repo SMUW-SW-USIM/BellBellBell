8ee505124c561db2fd04050aabe88eef79fb343b
DaEunShim <10ekdms01@naver.com>
2020-02-06 17:20:16 +0900

    update retrofit_recordlist -de

--- app/src/main/java/com/example/usim/data/RecordListResponse.java
+++ app/src/main/java/com/example/usim/data/RecordListResponse.java
@@ -2,6 +2,9 @@ package com.example.usim.data;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecordListResponse {
     @SerializedName("status")
     private int status;
@@ -9,11 +12,25 @@ public class RecordListResponse {
     @SerializedName("message")
     private String message;
 
+    @SerializedName("data")
+    public List<recordlist> data = new ArrayList();
+
+    public class recordlist{
+        @SerializedName("r_name")
+        public String r_name;
+        @SerializedName("r_info")
+        public String r_info;
+    }
+
     public int getStatus() {
         return status;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public List<recordlist> getData() {
+        return data;
     }
 }
--- app/src/main/java/com/example/usim/network/ServiceApi.java
+++ app/src/main/java/com/example/usim/network/ServiceApi.java
@@ -1,23 +1,12 @@
 package com.example.usim.network;
 
-import com.example.usim.data.ContactListAppendData;
-import com.example.usim.data.ContactListAppendResponse;
-import com.example.usim.data.ContactListData;
-import com.example.usim.data.ContactListResponse;
 import com.example.usim.data.IdCheckData;
 import com.example.usim.data.IdCheckResponse;
-import com.example.usim.data.RecordListAppendData;
-import com.example.usim.data.RecordListAppendResponse;
-import com.example.usim.data.RecordListData;
 import com.example.usim.data.RecordListResponse;
 import com.example.usim.data.SigninData;
 import com.example.usim.data.SigninResponse;
 import com.example.usim.data.SignupData;
 import com.example.usim.data.SignupResponse;
-import com.example.usim.data.VisitorCurrentData;
-import com.example.usim.data.VisitorCurrentResponse;
-import com.example.usim.data.VisitorListAppendData;
-import com.example.usim.data.VisitorListAppendResponse;
 import com.example.usim.data.VisitorListResponse;
 
 import retrofit2.Call;
@@ -43,24 +32,24 @@ public interface ServiceApi {
     @GET("/visitors/list")
     Call<VisitorListResponse> visitorList(@Header("token") String token);
 
-    // 실시간 방문자 정보
-    @GET("/visitors/current")
-    Call<VisitorCurrentResponse> visitorCurrent(@Body VisitorCurrentData data);
-
-    // 지인 버튼 누르면 방문자 리스트 추가
-    @POST("/visitors/list")
-    Call<VisitorListAppendResponse> postVisitorList(@Body VisitorListAppendData data);
+//    // 실시간 방문자 정보
+//    @GET("/visitors/current")
+//    Call<VisitorCurrentResponse> visitorCurrent(@Body VisitorCurrentData data);
+//
+//    // 지인 버튼 누르면 방문자 리스트 추가
+//    @POST("/visitors/list")
+//    Call<VisitorListAppendResponse> postVisitorList(@Body VisitorListAppendData data);
 
     // 녹음 리스트
     @GET("/records/list")
-    Call<RecordListResponse> recordList(@Body RecordListData data);
-    @POST("/records/list")
-    Call<RecordListAppendResponse> postRecordList(@Body RecordListAppendData data);
+    Call<RecordListResponse> recordList(@Header("token") String token);
+//    @POST("/records/list")
+//    Call<RecordListAppendResponse> postRecordList(@Body RecordListAppendData data);
 
-    // 긴급전화 리스트
-    @GET("/contacts/list")
-    Call<ContactListResponse> contactList(@Body ContactListData data);
-    @POST("/contacts/list")
-    Call<ContactListAppendResponse> postContactList(@Body ContactListAppendData data);
+//    // 긴급전화 리스트
+//    @GET("/contacts/list")
+//    Call<ContactListResponse> contactList(@Body ContactListData data);
+//    @POST("/contacts/list")
+//    Call<ContactListAppendResponse> postContactList(@Body ContactListAppendData data);
 
 }
